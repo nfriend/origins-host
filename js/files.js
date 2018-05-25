0f9c0e8665965894b5091333c2531d1ce5770eb5
Nathan Friend <hello@nathanfriend.io>
2018-05-25 17:15:58 -0300

    more

--- js/files.js
+++ js/files.js
@@ -1,7 +1,8 @@
 window.files = [
     {
         name: 'ADVNTR2.BAS',
-        description: 'Version 2 (or maybe 3?) of an adventure game.  This one didn\'t make it very far - in fact, it does nothing except print out a couple paragraphs of backstory.',
+        description:
+            "Version 2 (or maybe 3?) of an adventure game.  This one didn't make it very far - in fact, it does nothing except print out a couple paragraphs of backstory.",
         command: 'ADVNTR2.BAT'
     },
     {
@@ -16,57 +17,68 @@ window.files = [
     },
     {
         name: 'ALNATTCK.BAS',
-        description: 'A short animation of an alien ship kidnapping a little man.',
+        description:
+            'A short animation of an alien ship kidnapping a little man.',
         command: 'ALNATTCK.BAT'
     },
     {
         name: 'ANGLES.BAS',
-        description: 'A simple game (?) in which a player uses the "4" and "6" keys to control a moving line on the screen.  If I remember correctly, this was a proof-of-concept for a two-player snake-like game I planned on building.',
+        description:
+            'A simple game (?) in which a player uses the "4" and "6" keys to control a moving line on the screen.  If I remember correctly, this was a proof-of-concept for a two-player snake-like game I planned on building.',
         command: 'ANGLES.BAT'
     },
     {
         name: 'ANIMA.BAS',
-        description: 'A Pong clone.  Not written by me - written by an uncle who introduced me to QBasic.  Use "8" and "2" to move the paddle.',
+        description:
+            'A Pong clone.  Not written by me - written by an uncle who introduced me to QBasic.  Use "8" and "2" to move the paddle.',
         command: 'ANIMA.BAT'
     },
     {
         name: 'AROWKEYS.BAS',
-        description: 'A proof-of-concept program for interacting with the arrow keys.  After configuring the keys, a small circle can be moved around the screen using the selected keys.',
+        description:
+            'A proof-of-concept program for interacting with the arrow keys.  After configuring the keys, a small circle can be moved around the screen using the selected keys.',
         command: 'AROWKEYS.BAT'
     },
     {
         name: 'BACH.BAS',
-        description: 'The right-hand portion of Bach\'s "Menuet 1", a beginner piano piece.  This program was designed to be played at exactly the same time as "BACHLEFT.BAS" - the left-hand portion of the piece.',
+        description:
+            'The right-hand portion of Bach\'s "Menuet 1", a beginner piano piece.  This program was designed to be played at exactly the same time as "BACHLEFT.BAS" - the left-hand portion of the piece.',
         command: 'BACH.BAT'
     },
     {
         name: 'BACHLEFT.BAS',
-        description: 'The left-hand portion of Bach\'s "Menuet 1", a beginner piano piece.  This program was designed to be played at exactly the same time as "BACH.BAS" - the right-hand portion of the piece.',
+        description:
+            'The left-hand portion of Bach\'s "Menuet 1", a beginner piano piece.  This program was designed to be played at exactly the same time as "BACH.BAS" - the right-hand portion of the piece.',
         command: 'BACHLEFT.BAT'
     },
     {
         name: 'BATLSHIP.BAS',
-        description: 'The beginnings of a Battleship game.  This one never made it past printing the intial game board.',
+        description:
+            'The beginnings of a Battleship game.  This one never made it past printing the intial game board.',
         command: 'BATLSHIP.BAT'
     },
     {
         name: 'CAREER.BAS',
-        description: 'A career simulator in which the player chooses to be a shop keeper, a janitor, a construction worker, or a carpenter.  Unfortunately this game doesn\'t seem to do much more than ask you your name and exit.',
+        description:
+            "A career simulator in which the player chooses to be a shop keeper, a janitor, a construction worker, or a carpenter.  Unfortunately this game doesn't seem to do much more than ask you your name and exit.",
         command: 'CAREER.BAT'
     },
     {
         name: 'CELOHELP.BAS',
-        description: 'A flash-card game for cello students.  Users can be quized on their knowledge of bass clef, tenor clef, or treble clef.  See if you can make the high score list!',
+        description:
+            'A flash-card game for cello students.  Users can be quized on their knowledge of bass clef, tenor clef, or treble clef.  See if you can make the high score list!',
         command: 'CELOHELP.BAT'
     },
     {
         name: 'CELOHLP2.BAS',
-        description: 'The much anticipated sequel to Cello Help I.  This version adds a "problem notes" feature as well some fancier graphics to indicate correct and incorrect guesses.',
+        description:
+            'The much anticipated sequel to Cello Help I.  This version adds a "problem notes" feature as well some fancier graphics to indicate correct and incorrect guesses.',
         command: 'CELOHLP2.BAT'
     },
     {
         name: 'CHIMCLOK.BAS',
-        description: 'Draws an analog clock to the screen, complete with chiming sounds.  Unfortunately, this program runs much too slow on this platform.' ,
+        description:
+            'Draws an analog clock to the screen, complete with chiming sounds.  Unfortunately, this program runs much too slow on this platform.',
         command: 'CHIMCLOK.BAT'
     },
     // {
@@ -76,12 +88,14 @@ window.files = [
     // },
     {
         name: 'CLOCK.BAS',
-        description: 'Draws an analog clock to the screen.  Unfortunately, this program runs much too slow on this platform.',
+        description:
+            'Draws an analog clock to the screen.  Unfortunately, this program runs much too slow on this platform.',
         command: 'CLOCK.BAT'
     },
     {
         name: 'CNVRSNS.BAS',
-        description: 'A unit conversion utility.  Doesns\'t seem to work very well - 57 millimeters equals 57 inches??',
+        description:
+            "A unit conversion utility.  Doesns't seem to work very well - 57 millimeters equals 57 inches??",
         command: 'CNVRSNS.BAT'
     },
     // {
@@ -101,17 +115,20 @@ window.files = [
     // },
     {
         name: 'CODEMAKR.BAS',
-        description: 'A secret code creator program.  It asks the user for each character\'s mapping, and then prints the pretty-prints the decryption key to the screen.',
+        description:
+            "A secret code creator program.  It asks the user for each character's mapping, and then prints the pretty-prints the decryption key to the screen.",
         command: 'CODEMAKR.BAT'
     },
     {
         name: 'COOL.BAS',
-        description: 'One of my favorite programs.  This program draws intricate esigns to the screen using the color palette of the user\'s choice.  Users can create their own color palettes, which are saved to the filesystem for later use.',
+        description:
+            "One of my favorite programs.  This program draws intricate esigns to the screen using the color palette of the user's choice.  Users can create their own color palettes, which are saved to the filesystem for later use.",
         command: 'COOL.BAT'
     },
     {
         name: 'CULUR.BAS',
-        description: 'A colorful animation reminiscent of the old Microsoft 3D Pipes screensaver. Press "q" to quit.',
+        description:
+            'A colorful animation reminiscent of the old Microsoft 3D Pipes screensaver. Press "q" to quit.',
         command: 'CULUR.BAT'
     },
     // {
@@ -121,42 +138,49 @@ window.files = [
     // },
     {
         name: 'DRAW!.BAS',
-        description: 'Another one of my favorite programs - you can tell I enjoyed working on this one by the number of arcane commands I built into the application.  This program is essentially a very confusing version of Microsoft Paint.  If you can get past the wall of text describing how to use the program, it\'s actually pretty fun!',
+        description:
+            "Another one of my favorite programs - you can tell I enjoyed working on this one by the number of arcane commands I built into the application.  This program is essentially a very confusing version of Microsoft Paint.  If you can get past the wall of text describing how to use the program, it's actually pretty fun!",
         command: 'DRAW!.BAT'
     },
     {
         name: 'ECODER.BAS',
-        description: 'An encoder program that uses the provided password to encode a secret message.  Unfortunately there doesn\'t seem to be a way to <em>de-</em>code the message after it\'s saved to the files system...<br /><br />Handy tip: press "esc" to stop recording your secret message.',
+        description:
+            'An encoder program that uses the provided password to encode a secret message.  Unfortunately there doesn\'t seem to be a way to <em>de-</em>code the message after it\'s saved to the files system...<br /><br />Handy tip: press "esc" to stop recording your secret message.',
         command: 'ECODER.BAT'
     },
     {
         name: 'EXACTAGE.BAS',
-        description: '',
+        description:
+            'A program that determines your <em>exact</em> age, down to the second. (Provided you know the exact hour, minute, and second of your birth!)',
         command: 'EXACTAGE.BAT'
     },
     {
         name: 'FACTORS.BAS',
-        description: '',
+        description:
+            'A small program that uses a brute-force algorithm to determine whether a number is prime or composite, and lists all of its factors.<br /><br />Pro tip: numbers over 100 take a <em>long</em> time to process.',
         command: 'FACTORS.BAT'
     },
     {
         name: 'FALLING!.BAS',
-        description: '',
+        description:
+            'An ASCII animation of a small character falling, complete with sound effects.  This was originally going to be a cutscene for my ADVNTURE.BAS game.',
         command: 'FALLING!.BAT'
     },
-    {
-        name: 'FIGHT.BAS',
-        description: '',
-        command: 'FIGHT.BAT'
-    },
+    // {
+    //     name: 'FIGHT.BAS',
+    //     description: '',
+    //     command: 'FIGHT.BAT'
+    // },
     {
         name: "FILM'UP.BAS",
-        description: '',
-        command: 'UP.BAS", description:T'
+        description:
+            'A simple drawing program that emulates the "paint bucket fill" feature in Microsoft Paint.  Unfortunately, this program runs a little too slow in this environment to be usable.<br /><br />Use "5", "1", "2", and "3" to move your cursor, but be patient - it takes quite a while for the screen to update.  Press "space" to fill the current area and "q" to quit.  Also - press one of the directional keys ("5", "1", "2", or "3") once the program starts to initialize the circles.',
+        command: "FILM'UP.BAT"
     },
     {
         name: 'FPOSES.BAS',
-        description: '',
+        description:
+            'A fighting simulator, for eventual use in my ADVNTURE.BAS game.  Another program that didn\'t make it much farther than the visual effects stage.  Use "a", "q", "l", "o", "1", "2", "7", and "8" to perform different moves.  Press "j" twice to quit.',
         command: 'FPOSES.BAT'
     },
     {
@@ -346,7 +370,8 @@ window.files = [
     },
     {
         name: 'STRESS.BAS',
-        description: 'A stress-relieving program in which you press any key to punch a representation of the person you are currently mad at, resulting in their eventual spontaneous combustion.<br /><br />  However, I made sure that <em>my</em> name could never be the target.',
+        description:
+            'A stress-relieving program in which you press any key to punch a representation of the person you are currently mad at, resulting in their eventual spontaneous combustion.<br /><br />  However, I made sure that <em>my</em> name could never be the target.',
         command: 'STRESS.BAT'
     },
     {
